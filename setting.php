c41302a10efed38305455ab94ececa92d21ea02e
Coelens <cervarius45@gmail.com>
2026-04-18 20:55:01 +0700

    setting update

--- setting.php
+++ setting.php
@@ -1,25 +1,51 @@
+<?php 
+if(isset($_POST['kirim'])) {
+    $txtdate = $_POST['txtdate'];
+    $txtarah = $_POST['txtarah'];
+    
+    setcookie("urut_berdasarkan", $txtdate, time() + (86400 * 30), "/");
+    setcookie("arah", $txtarah, time() + (86400 * 30), "/");
+    
+    header("Location: setting.php?sukses=1");
+    exit();
+}
+
+$urut_aktif = isset($_COOKIE['urut_berdasarkan']) ? $_COOKIE['urut_berdasarkan'] : "Tanggal";
+$arah_aktif = isset($_COOKIE['arah']) ? $_COOKIE['arah'] : "Asc";
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project WEBPROG</title>
 </head>
-<body class="body">
+<body>
     <h1>Setting</h1>
-    <form action="index.php" method="post">
-        <p><label>Urut Berdasarkan : </label>
-            <label><input type="radio" name="txtdate" id="tanggal" value="Tanggal" checked>Tanggal </label>
-            <label><input type="radio" name="txtdate" id="nominal" value="Nominal">Nominal </label></p>
+    
+    <?php 
+    if(isset($_GET['sukses']) && $_GET['sukses'] == '1') {
+        echo "<p>Data berhasil disimpan</p>";
+    }
+    ?>
 
-        <p><label>Arah Berdasarkan : </label>
-            <label><input type="radio" name="txtarah" id="arah" value="Asc" checked>Ascending </label>
-            <label><input type="radio" name="txtarah" id="arah" value="Desc">Descending </label></p>
+    <form method="post" action="setting.php">
+        <p> <label>Urut Berdasarkan : </label>
+        <label> <input type="radio" name="txtdate" value="Tanggal" <?php if($urut_aktif == "Tanggal") echo "checked"; ?>> Tanggal</label>
+        <input type="radio" name="txtdate" id="nominal" value="Nominal" <?php if($urut_aktif == "Nominal") echo "checked"; ?>> <label for="nominal"> Nominal</label>
+        </p>
         
-    <input type="submit" name="kirim" text="kirim">
+        <p> <label>Arah : </label>
+        <label> <input type="radio" name="txtarah" value="Asc" <?php if($arah_aktif == "Asc") echo "checked"; ?>> Ascending</label>
+        <input type="radio" name="txtarah" id="desc" value="Desc" <?php if($arah_aktif == "Desc") echo "checked"; ?>> <label for="desc"> Descending</label>
+        </p>
+        
+        <p>
+            <input type="submit" name="kirim" value="Simpan">
+        </p>
     </form>
-
+    
     <p></p>
-    <a href="index.php"><< Kembali</a>  
+    <a href="index.php">&laquo; Kembali</a>
 </body>
 </html>
